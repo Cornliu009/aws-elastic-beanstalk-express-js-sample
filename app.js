bf9cbcf219cf24f990a7251d9bfd5fd450877f9d
Cornliu009 <corneliu.dodita@iis.utm.md>
2021-03-17 22:26:34 +0200

    Testing assumpion.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const app2 = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('It works? What kind of magic is that???!!!'));
-app.get('/', (req, res) => res.send('Testing DevOps CI/CD!!!'));
+app2.get('/', (req, res) => res.send('Testing DevOps CI/CD!!!'));
 
 
 app.listen(port);
