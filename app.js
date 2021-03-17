c5048046628048865d48da20314f65365bbdfb35
Cornliu009 <corneliu.dodita@iis.utm.md>
2021-03-17 22:22:47 +0200

    Changing places

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing DevOps CI/CD!!!'));
 app.get('/', (req, res) => res.send('It works? What kind of magic is that???!!!'));
+app.get('/', (req, res) => res.send('Testing DevOps CI/CD!!!'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
