2660a1239781657a321ca272abd7240f6639ed90
Cornliu009 <corneliu.dodita@iis.utm.md>
2021-03-17 22:18:01 +0200

    Like Magicgit add app.js !

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Testing DevOps CI/CD!!!'));
+app.get('/', (req, res) => res.send('It works? What kind of magic is that???!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
