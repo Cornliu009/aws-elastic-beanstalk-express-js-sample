30785857620dd6a0d2b6018a60bf607d0ae30b2f
Cornliu009 <corneliu.dodita@iis.utm.md>
2021-03-18 08:51:12 +0200

    Testing if works.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Full Pipeline Test!!!'));
+app.get('/', (req, res) => res.send('Full Pipeline Testing!!!'));
 
 app.listen(port);
 
